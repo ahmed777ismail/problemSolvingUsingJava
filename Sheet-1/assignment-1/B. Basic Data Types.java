ec7d7aa7f04b9ba1de3b23628b65c6521ef688e0
ahmed777ismail <ahmed1kaballo@gmail.com>
2025-07-19 20:53:41 +0300

    Add basic data types and simple calculator implementations

---
+++ Sheet-1/assignment-1/B. Basic Data Types.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i = sc.nextInt();
+        long l = sc.nextLong();
+        char c = sc.next().charAt(0);
+        float f = sc.nextFloat();
+        double d = sc.nextDouble();
+
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(c);
+        System.out.println(f);
+        System.out.println(d);
+    }
+}
